f3cc4306f54420bd257ff128ecf0e74dca3ea1b8
Ullash2401 <ullash.cse.20220104034@aust.edu>
2025-12-27 11:55:30 +0600

    Initial commit: project setup with backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend running successfully!");
+});
+
+// MongoDB connection + server start
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () => {
+      console.log("Server running on port 5000");
+    });
+  })
+  .catch(err => console.error(err));
