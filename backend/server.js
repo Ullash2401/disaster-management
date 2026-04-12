28f744263480639e3ff639307fe40454a42be70a
Ullash2401 <ullash.cse.20220104034@aust.edu>
2026-04-12 07:46:53 +0600

    update

--- backend/server.js
+++ backend/server.js
@@ -3,29 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-<<<<<<< HEAD
-const app = express();
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend running successfully!");
-});
-
-// MongoDB connection + server start
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
-  })
-  .catch(err => console.error(err));
-=======
 // routes
 const authRoutes = require("./routes/auth");
 const reportRoutes = require("./routes/reportRoutes");
@@ -101,4 +78,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
->>>>>>> dev
